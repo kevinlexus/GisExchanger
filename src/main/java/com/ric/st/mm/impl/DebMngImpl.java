8f45f8041f36c31da0f70835bc0b5b7839e70f7a
Leo <factor@mail.ru>
2018-06-07 09:08:42 +0700

    Поправил один класс

--- src/main/java/com/ric/st/mm/impl/DebMngImpl.java
+++ src/main/java/com/ric/st/mm/impl/DebMngImpl.java
@@ -1,64 +1,21 @@
 package com.ric.st.mm.impl;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiElementFieldType;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiElementNsiRefFieldType;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiElementNsiRefFieldType.NsiRef;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiElementStringFieldType;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiElementType;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiItemInfoType;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiItemType;
-import ru.gosuslugi.dom.schema.integration.nsi_base.NsiListType;
-import ru.gosuslugi.dom.schema.integration.nsi_common_service.Fault;
-
-import com.dic.bill.dao.AchargeDAO;
 import com.dic.bill.dao.ApenyaDAO;
 import com.dic.bill.dao.SaldoUslDAO;
-import com.diffplug.common.base.Errors;
-import com.ric.cmn.Utl;
 import com.ric.bill.dao.AflowDAO;
-import com.ric.bill.dao.ChrgDAO;
-import com.ric.bill.dao.TempLskDAO;
 import com.ric.bill.dao.hotora.SaldoDAO;
-import com.ric.bill.dto.OrgDTO;
-import com.ric.bill.dto.SumChrgRec;
-import com.ric.bill.dto.SumDebRec;
-import com.ric.bill.model.bs.Org;
 import com.ric.bill.model.exs.Eolink;
-import com.ric.bill.model.exs.Ulist;
-import com.ric.bill.model.exs.UlistTp;
-import com.ric.bill.model.hotora.oralv.SchetContragent;
 import com.ric.bill.model.oralv.Ko;
-import com.ric.st.builder.NsiCommonAsyncBindingBuilders;
-import com.ric.st.builder.NsiServiceAsyncBindingBuilders;
-import com.ric.st.builder.impl.NsiCommonAsyncBindingBuilder;
-import com.ric.st.dao.UlistDAO;
-import com.ric.st.excp.CantGetNSI;
-import com.ric.st.excp.CantSendSoap;
-import com.ric.st.excp.CantSignSoap;
-import com.ric.st.excp.CantUpdNSI;
-import com.ric.st.impl.RefStore;
-import com.ric.st.impl.SoapConfig;
-import com.ric.st.mm.ChrgMng;
+import com.ric.cmn.Utl;
 import com.ric.st.mm.DebMng;
-import com.ric.st.mm.OrgMng;
-import com.ric.st.mm.UlistMng;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Сервис получения данных о задолженности, пени из разных источников
@@ -112,7 +69,7 @@ public class DebMngImpl implements DebMng {
 	 * @param ko - Ko объект лиц.счета (обязательно для новой разраб.)
 	 * @param period - период выборки
 	 * @param uk - УК из Eolink
-	 *  
+	 *
 	 */
 	@Override
 	public BigDecimal getPenAmnt(String lsk, Ko ko, String period, Eolink uk) {
@@ -136,9 +93,9 @@ public class DebMngImpl implements DebMng {
 			// получить всю пеню по лиц.счету
 			penMain = apenyaDao.getPenAmnt(lsk, lastDate);
 		}
-		
+
 		return penMain;
-	} 
+	}
 
 
 
