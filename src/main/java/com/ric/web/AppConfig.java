7aaa61ae17aff7d04fc34a7fbe566f55b30c97b5
lev <factor@mail.ru>
2018-06-25 19:06:15 +0600

    Resource

--- src/main/java/com/ric/web/AppConfig.java
+++ src/main/java/com/ric/web/AppConfig.java
@@ -4,16 +4,12 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -27,14 +23,12 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.ric.st.impl.TaskController;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @ComponentScan({"com.ric.st", "com.ric.bill.dao", "com.dic.bill", "com.dic.bill.dao", "com.ric.bill.dao.hotora",
 	"com.ric.bill.mm", "com.ric.bill", "com.ric.st.builder"}) // это нужно чтобы работали Unit-тесты! (по сути можно закомментить)
-@ImportResource("spring.xml")
+@ImportResource({"res/spring.xml"})
 @EnableCaching
 @EnableJpaRepositories(basePackages= {"com.ric.bill.dao", "com.dic.bill.dao", "com.ric.bill.dao.hotora"})
 @EnableScheduling
@@ -45,11 +39,12 @@ public class AppConfig  implements ApplicationContextAware {
 
 	@Autowired
 	DataSource ds;
-	
+
+	@Override
 	public void setApplicationContext(ApplicationContext context) throws BeansException {
 		ctx = context;
 	}
-	
+
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 	    // JPA settings
@@ -68,7 +63,7 @@ public class AppConfig  implements ApplicationContextAware {
 	    factory.afterPropertiesSet();
 	    return factory;
 	}
-	
+
 	@Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         return new RabbitTemplate(connectionFactory);
